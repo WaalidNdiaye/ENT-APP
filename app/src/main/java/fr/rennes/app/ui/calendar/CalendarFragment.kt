4ee4a50c07f6871b6635ae884db60a2b78fac25e
waali_ndiaye <tangi.lejeune@gmail.com>
2023-05-18 15:50:48 +0200

    Calendar View init
    implementation of default view for calendar fragment
    
    import and use of repo :https://github.com/islandparadise14/MinTimetable

--- app/src/main/java/fr/rennes/app/ui/calendar/CalendarFragment.kt
+++ app/src/main/java/fr/rennes/app/ui/calendar/CalendarFragment.kt
@@ -1,14 +1,22 @@
 package fr.rennes.app.ui.calendar
 
-import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.CalendarView
+import android.widget.Toast
+import com.islandparadise14.mintable.MinTimeTableView
+import com.islandparadise14.mintable.model.ScheduleDay
+import com.islandparadise14.mintable.model.ScheduleEntity
 import fr.rennes.app.R
 
 class CalendarFragment : Fragment() {
+
+    private val day = arrayOf("Mon", "Tue", "Wen", "Thu", "Fri","Sat")
+    private val scheduleList: ArrayList<ScheduleEntity> = ArrayList()
+
 
     companion object {
         fun newInstance() = CalendarFragment()
@@ -17,16 +25,44 @@ class CalendarFragment : Fragment() {
     private lateinit var viewModel: CalendarViewModel
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_calendar, container, false)
+
+        val view = inflater.inflate(R.layout.fragment_calendar, container, false)
+
+        /** Default calendar */
+        val simpleCalendarView = view.findViewById<CalendarView>(R.id.default_calendar)
+        simpleCalendarView.setOnDateChangeListener{ _, year, month, dayOfMonth ->
+            // Note that months are indexed from 0. So, 0 means January, 1 means february, 2 means march etc.
+            val msg = "Selected date is " + dayOfMonth + "/" + (month + 1) + "/" + year
+            Toast.makeText(activity, msg, Toast.LENGTH_SHORT).show()
+        }
+
+        /** MinTimeTable */
+        val table = view.findViewById<MinTimeTableView>(R.id.MinTimeTableView)
+        val schedule = ScheduleEntity(
+            32, //originId
+            "Database", //scheduleName
+            "IT Building 301", //roomInfo
+            ScheduleDay.TUESDAY, //ScheduleDay object (MONDAY ~ SUNDAY)
+            "8:20", //startTime format: "HH:mm"
+            "10:30", //endTime  format: "HH:mm"
+            "#73fcae68", //backgroundColor (optional)
+            "#000000" //textcolor (optional)
+        )
+        scheduleList.add(schedule)
+        table.initTable(day)
+        table.baseSetting(100,100,50)
+        table.updateSchedules(scheduleList)
+
+
+
+        return view
     }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(CalendarViewModel::class.java)
-        // TODO: Use the ViewModel
-    }
+
+
 
 }
